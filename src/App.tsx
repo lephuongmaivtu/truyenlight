8bb727a3dad8047fa7f1830a2d03e869d0769208
lephuongmaivtu <lephuongmaivtu@gmail.com>
2025-10-14 20:26:13 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Header } from './components/Header';
-import { Footer } from './components/Footer';
-import { Homepage } from './components/Homepage';
-import { StoryDetail } from './components/StoryDetail';
-import { ChapterReader } from './components/ChapterReader';
-import { ReadingProvider } from './components/ReadingProvider';
-
-// 👇 import đúng 1 lần thôi
-import { ProfilePage } from './pages/ProfilePage';
-import { LoginPage } from './pages/LoginPage';
-import { RegisterPage } from './pages/RegisterPage';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
+import { Homepage } from "./components/Homepage";
+import { StoryDetail } from "./components/StoryDetail";
+import { ChapterReader } from "./components/ChapterReader";
+import { ReadingProvider } from "./components/ReadingProvider";
+import { ProfilePage } from "./pages/ProfilePage";
+import { LoginPage } from "./pages/LoginPage";
+import { RegisterPage } from "./pages/RegisterPage";
 import { usePageTracking } from "./hooks/usePageTracking";
 
 function App() {
-  usePageTracking(); // 👉 thêm dòng này
+  usePageTracking(); // ✅ Chỉ cần gọi ở đây là đủ
 
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/story/:slug" element={<StoryPage />} />
-      </Routes>
-    </Router>
-  );
-}
-
-export default App;
-
-export default function App() {
   return (
     <ReadingProvider>
       <Router>
@@ -39,8 +24,6 @@ export default function App() {
               <Route path="/" element={<Homepage />} />
               <Route path="/story/:slug" element={<StoryDetail />} />
               <Route path="/story/:slug/:chapterSlug" element={<ChapterReader />} />
-
-              {/* 👇 route mới */}
               <Route path="/profile" element={<ProfilePage />} />
               <Route path="/login" element={<LoginPage />} />
               <Route path="/register" element={<RegisterPage />} />
@@ -52,3 +35,5 @@ export default function App() {
     </ReadingProvider>
   );
 }
+
+export default App;
