e0c112f0c0883a439b9cb459ba074fab264b2b94
lephuongmaivtu <lephuongmaivtu@gmail.com>
2025-10-14 20:37:57 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,31 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Header } from "./components/Header";
-import { Footer } from "./components/Footer";
-import { Homepage } from "./components/Homepage";
-import { StoryDetail } from "./components/StoryDetail";
-import { ChapterReader } from "./components/ChapterReader";
-import { ReadingProvider } from "./components/ReadingProvider";
-import { ProfilePage } from "./pages/ProfilePage";
-import { LoginPage } from "./pages/LoginPage";
-import { RegisterPage } from "./pages/RegisterPage";
-import { usePageTracking } from "./hooks/usePageTracking";
-
 function App() {
-  usePageTracking(); // ✅ Chỉ cần gọi ở đây là đủ
-
   return (
     <ReadingProvider>
       <Router>
-        <div className="min-h-screen bg-background flex flex-col">
-          <Header />
-          <main className="flex-1">
-            <Routes>
-              <Route path="/" element={<Homepage />} />
-              <Route path="/story/:slug" element={<StoryDetail />} />
-              <Route path="/story/:slug/:chapterSlug" element={<ChapterReader />} />
-              <Route path="/profile" element={<ProfilePage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/register" element={<RegisterPage />} />
-            </Routes>
-          </main>
-          <Footer />
-        </div>
+        {/** ✅ Gọi hook ở đây, bên trong Router */}
+        <PageTrackerAndRoutes />
       </Router>
     </ReadingProvider>
   );
 }
 
-export default App;
+function PageTrackerAndRoutes() {
+  usePageTracking();
+
+  return (
+    <div className="min-h-screen bg-background flex flex-col">
+      <Header />
+      <main className="flex-1">
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/story/:slug" element={<StoryDetail />} />
+          <Route path="/story/:slug/:chapterSlug" element={<ChapterReader />} />
+          <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </main>
+      <Footer />
+    </div>
+  );
+}
