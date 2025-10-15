4231c37c814f922d959aad1ef8a37896a3604dc0
lephuongmaivtu <lephuongmaivtu@gmail.com>
2025-10-15 19:57:27 +0700

    Update Homepage.tsx

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -581,17 +581,25 @@ return (
 
         {/* 🔸 CỘT PHẢI giữ nguyên */}
         <div className="space-y-6">
-     <CardContent className="overflow-hidden max-w-full">
-      <iframe
-        src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ftruyenlight&tabs=&width=340&height=180&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true"
-        width="100%"
-        height="180"
-        style={{ border: "none", overflow: "hidden", borderRadius: "12px" }}
-        scrolling="no"
-        frameBorder="0"
-        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-      ></iframe>
-    </CardContent>
+    {/* Theo dõi fanpage */}
+      <section className="w-full md:w-[340px] mx-auto md:mx-0">
+        <div className="rounded-xl border bg-card shadow-md overflow-hidden">
+          <iframe
+            src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ftruyenlight&tabs=&width=340&height=160&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=true"
+            width="100%"
+            height="160"
+            style={{
+              border: "none",
+              overflow: "hidden",
+              display: "block",
+            }}
+            scrolling="no"
+            frameBorder="0"
+            allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+          ></iframe>
+        </div>
+      </section>
+
 
 
 
