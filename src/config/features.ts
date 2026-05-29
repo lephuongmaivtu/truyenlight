0e473e272079d13460f636740b988f38cf2274a6
lephuongmaivtu <lephuongmaivtu@gmail.com>
2026-05-29 18:14:08 +0700

    Create features.ts

---
+++ src/config/features.ts
@@ -0,0 +1,13 @@
+export const FEATURES = {
+  // Homepage
+  DAILY_CHECKIN: false,
+
+  // Header
+  REWARD_SHOP: false,
+  COINS: false,
+
+  // Profile
+  PROFILE_21DAY_GIFT: false,
+  PROFILE_READER_TASKS: false,
+  PROFILE_PURCHASED: false,
+};
